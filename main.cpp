7d353e9113d329933b6d3e61af93b2b9942f6f5e
FrancoSimonFormia <franco.formia@alumnos.frgp.utn.edu.ar>
2023-10-16 09:45:48 -0300

    Los constructores toman los datos del sistema. Se cambia getDiaSemana()

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 
 using namespace std;
-#include "FechaHorario.h"
+
 
 int main()
 {
 
-    FechaHorario aux(15,2,2018,0,0,12);
-
-    cout << aux.toString() << endl;
-
-    cout << aux.getFecha().getNombreDia(aux.getFecha());
     return 0;
 }
