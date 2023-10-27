638bf4aa3953657aeec35582b21c10a15362c570
Lucho <luchoguit@gmail.com>
2023-10-27 18:49:18 -0300

    Creada la rama, agregados los archivos

---
+++ GestorMaterias.h
@@ -0,0 +1,14 @@
+#ifndef GESTORMATERIAS_H
+#define GESTORMATERIAS_H
+
+
+class GestorMaterias
+{
+private:
+public:
+    GestorMaterias();
+
+
+};
+
+#endif // GESTORMATERIAS_H
--- main.cpp
+++ main.cpp
@@ -2,10 +2,6 @@
 
 using namespace std;
 
-#include "Fecha.h"
-#include "Menu.h"
-
-
 int main()
 {
     return 0;
