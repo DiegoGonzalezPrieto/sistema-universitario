ead70545a1f448d8e1ca4f69dfec22b3f6191b11
Diego González Prieto <diegogp89@gmail.com>
2023-11-11 09:26:22 -0300

    reestablecido main

--- main.cpp
+++ main.cpp
@@ -1,31 +1,11 @@
 #include <iostream>
 using namespace std;
-#include "GestorEventos.h"
-#include "Menu.h"
-#include "GestorMaterias.h"
-#include "GestorCursadaMateria.h"
+#include "Sistema.h"
 
 int main()
 {
-    GestorCursadaMateria g("sistema/datos/cursada_materias.dat", "sistema/datos/materias.dat");
-    g.iniciar();
+    Sistema sistema;
+    sistema.iniciar();
 
-
-
-//cout << vec[1].toString()<< endl;
-
-//EstadoMateria e;
-//cout << (int)g.seleccionarEstadoCursadaMateria(e);
-//cout<< endl<< "seleccion: "<< e;
-
-//    Menu m({"Uno", "Dos", "Tres"});
-
-//    cout << "Resultado : "<< m.mostrar();
-//
-//float a = obtenerNumeroDecimal();
-//string b;
-//cout << "VALOR: " << a<< endl;
-//cin >> b;
-//cout << "RESIDUO: "<< b;
     return 0;
 }
